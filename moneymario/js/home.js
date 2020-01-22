4a7bd4a5736cb4b1bf0c6974a8f91f50c65358a3
N10T <andy.tisba@gmail.com>
2020-01-22 01:01:55 +0100

    2nd page almost finished

--- moneymario/js/home.js
+++ moneymario/js/home.js
@@ -1,8 +1,20 @@
+var explainPage;
+axios
+  .get("./views/explain.html")
+  .then(response => {
+    explainPage = response.data;
+    // Here we can do something with the response object
+  })
+  .catch(err => {
+    // Here we catch the error and display it
+  });
+const body = document.getElementById("all");
+const homeMusic = new Audio("./sounds/music-Title.mp3");
 (function start() {
   let score = 0;
   const coins = document.querySelectorAll(".coin");
   const title = document.getElementById("title");
-  const homeMusic = document.getElementById("home-music");
+
   title.onmouseover = function() {
     if (homeMusic.played) {
       homeMusic.play();
@@ -14,6 +26,11 @@
   coins.forEach(
     e =>
       (e.onclick = function(e) {
+        if (homeMusic.played) {
+          homeMusic.play();
+          homeMusic.loop = true;
+        }
+
         score += 1;
         const coinSound = new Audio(
           "https://freesound.org/data/previews/341/341695_5858296-lq.mp3"
@@ -25,8 +42,49 @@
         coinSound.play();
         e.target.classList.remove("coin");
         console.log(score);
-
-        if (score === 5) hereWeGoURL.play();
+        if (score === 5) {
+          hereWeGoURL.play();
+          // page out with sound
+          title.parentElement.classList.add("scale-out");
+          setTimeout(gonextpage, 500);
+          function gonextpage() {
+            body.innerHTML = explainPage;
+            refreshPage2();
+          }
+        }
       })
   );
 })();
+
+function refreshPage2() {
+  const peopleCard = document.querySelectorAll("#container2 .people");
+  const container = document.getElementById("container2");
+  peopleCard.forEach(a => (a.onclick = cardCliked));
+  function cardCliked() {
+    //play sound
+  }
+
+  const moveTheBar = function(e) {
+    let position = 0;
+    var x = e.clientX;
+    position = x - window.innerWidth / 2;
+    if (position > 330) position = 330;
+    if (position < -330) position = -330;
+    container.style.transform = `translate(${position}px)`;
+  };
+  peopleCard.forEach(e => (e.onmousemove = moveTheBar));
+
+  const musicbox = document.getElementById("musicbox");
+  musicbox.onclick = () => {
+    if (homeMusic.paused) {
+      homeMusic.play();
+      musicbox.classList.add("musicplay");
+      musicbox.classList.remove("musicpause");
+    } else {
+      homeMusic.pause();
+      musicbox.classList.remove("musicplay");
+      musicbox.classList.add("musicpause");
+    }
+    homeMusic.loop = true;
+  };
+}
